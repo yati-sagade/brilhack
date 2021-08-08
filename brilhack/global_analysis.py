85f0107c8018e5b1bfdf2a25cfde7af04964b1be
Yati Sagade <yati@google.com>
2021-08-08 21:28:09 +0200

    Add funcs to find natural loops

--- brilhack/global_analysis.py
+++ brilhack/global_analysis.py
@@ -1,6 +1,7 @@
 from typing import List, Set
 from functools import reduce
 import logging
+from collections import deque
 
 from .basic_blocks import Function
 
@@ -56,7 +57,7 @@ def dominators(cfg: List[List[int]]) -> List[Set[int]]:
     preds = predecessor_map(cfg)
     niter = 0
     while more:
-        print('iter {}: {}'.format(niter, dom))
+        logging.debug('finding dominators iter {}: {}'.format(niter, dom))
         more = False
         niter += 1
         for i in range(nblock):
@@ -87,3 +88,108 @@ def dominator_tree(cfg: List[List[int]]) -> List[Set[int]]:
         if parent:
             domtree[parent.pop()].add(idx)
     return domtree
+
+
+class NotANaturalLoop(Exception):
+    pass
+
+
+def _extract_loop(cfg: List[List[int]], doms, preds, header, loopback):
+    """Given the header and a loopback node that there is a loopback->header
+  back-edge in `cfg`, returns the minimal set of nodes in `cfg` L such that
+
+    1. loopback and header are in L.
+    2. If a given node n is in L, and n != header, preds(n) are all also in L.
+  """
+    if header not in doms[loopback]:
+        raise NotANaturalLoop('{} is not dominated by header {}'.format(
+            loopback, header))
+    # If a node A dominates B, it also dominates each of preds[B].
+    # Since header dominates loopback, it also dominates all its preds and
+    # preds of those preds, etc.
+    loop = set()
+    q = deque([loopback])
+    while q:
+        node = q.popleft()
+        loop.add(node)
+        if node == header:
+            continue
+        assert header in doms[node],\
+          'Expected {} to be dominated by header {}'.format(node, header)
+        for pred in preds[node]:
+            if pred not in loop:
+                q.append(pred)
+    return loop
+
+
+def _dfs_cfg(cfg: List[List[int]],
+             on_node_visit=None,
+             on_node_process=None,
+             on_back_edge=None):
+    """Runs DFS on the given CFG.
+
+    Calls:
+      - on_node_visit(cfg, dominators, predecessors, node) the first time a
+      node is encountered.
+      - on_node_process(cfg, dominators, predecessors, node) when the DFS
+      for a node has finished.
+      - on_back_edge(cfg, dominators, predecessors, head, tail) for each
+      back-edge (tail->head).
+
+      In the above callbacks,
+      * dominators[i]: set = dominators of node i,
+      * predecessors[i]: list = preds of node i,
+    """
+
+    visited = set()
+    processed = set()
+    doms = dominators(cfg)
+    preds = predecessor_map(cfg)
+
+    def _dfs(idx):
+        visited.add(idx)
+        if on_node_visit is not None:
+            on_node_visit(cfg, doms, preds, idx)
+        for succ in cfg[idx]:
+            if succ not in visited:
+                _dfs(succ)
+            elif (succ in visited and succ not in processed
+                  and on_back_edge is not None):
+                on_back_edge(cfg, doms, preds, succ, idx)
+        processed.add(idx)
+        if on_node_process is not None:
+            on_node_process(cfg, doms, preds, idx)
+
+    for i in range(len(cfg)):
+        _dfs(i)
+
+
+def extract_natural_loops(cfg: List[List[int]]) -> List[List[int]]:
+    """Returns all natural loops in `cfg`."""
+    loops = []
+
+    def on_back_edge(cfg, doms, preds, header, curr):
+        try:
+            loop = _extract_loop(cfg, doms, preds, header, curr)
+            loops.append(loop)
+        except NotANaturalLoop:
+            pass
+
+    _dfs_cfg(cfg, on_back_edge=on_back_edge)
+    return loops
+
+
+def is_cfg_reducible(cfg: List[List[int]]) -> bool:
+    """Returns if the given CFG is reducible, i.e., if each back-edge in the
+    CFG forms a natural loop."""
+    is_reducible = True
+
+    def on_back_edge(cfg, doms, preds, header, curr):
+        nonlocal is_reducible
+        try:
+            loop = _extract_loop(cfg, doms, preds, header, curr)
+        except NotANaturalLoop:
+            is_reducible = False
+
+    _dfs_cfg(cfg, on_back_edge=on_back_edge)
+    return is_reducible
--- brilhack/global_analysis_test.py
+++ brilhack/global_analysis_test.py
@@ -1,6 +1,6 @@
 import unittest
 from .basic_blocks import BBProgram
-from .global_analysis import dominators, dominator_tree
+from .global_analysis import dominators, dominator_tree, extract_natural_loops, is_cfg_reducible
 
 
 class GlobalAnalysisTest(unittest.TestCase):
@@ -27,3 +27,20 @@ class GlobalAnalysisTest(unittest.TestCase):
             {6},
             set(),
         ])
+
+    def test_extract_natural_loops(self):
+        self.assertEqual(
+            extract_natural_loops(
+                cfg=[[1], [5, 2], [3, 4], [4], [1], [6], []]), [{1, 2, 3, 4}])
+
+        # This CFG is not reducible, since the only loop it contains is not
+        # a natural loop (node 3 forms a back-edge with header node 1, but
+        # there is an external 2->3 edge that makes the loop not natural).
+        self.assertEqual(extract_natural_loops(cfg=[[1, 2], [3], [3], [1]]),
+                         [])
+
+    def test_is_cfg_reducible(self):
+        self.assertTrue(
+            is_cfg_reducible(cfg=[[1], [5, 2], [3, 4], [4], [1], [6], []]))
+        self.assertFalse(is_cfg_reducible(cfg=[[1, 2], [2], [1]]))
+        self.assertFalse(is_cfg_reducible(cfg=[[1, 2], [3], [3], [1]]))
